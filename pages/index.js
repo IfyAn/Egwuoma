47ece60a54b406faa3967389eac9e345f968630a
Anyam Ifeanyi <chukwuanyan@gmail.com>
2022-02-10 11:20:11 +0100

    second commit

--- pages/index.js
+++ pages/index.js
@@ -19,9 +19,9 @@ export default function Home() {
   }
 
   return (
-    <div className="">
+    <div className="overflow-scroll overflow-y-auto h-screen w-screen">
       <Head>
-        <title>Spotify - Dashboard</title>
+        <title>Egwuoma - Dashboard</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Dashboard />
